0961a21c2540127e6e77499abcc9b36a67a0bff9
sRRRs <srrrs4510@gmail.com>
2022-08-14 15:22:49 +0900

    Tauri

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,6 +5,14 @@
 
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![
+      simple_command,
+    ])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn simple_command() {
+  println!("simple command is invoked");
 }
